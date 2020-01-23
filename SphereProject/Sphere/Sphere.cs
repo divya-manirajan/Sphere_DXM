57c638e9729f619ae18bbbe1d5fa214e8e0e295b
divya-manirajan <44423406+divya-manirajan@users.noreply.github.com>
2020-01-23 12:46:46 -0500

    Add files via upload

---
+++ SphereProject/Sphere/Sphere.cs
@@ -0,0 +1,85 @@
+﻿/**
+ * This class models a sphere object
+ * @author copied from Dr. Kelley
+ * @version 1.0
+ * Proj1-Sphere
+ * SP20
+ */
+using System;
+namespace Sphere
+{
+    public class Sphere
+    {
+        private double diameter;
+
+         /**
+	     * Empty-argument constructor to put object
+	     * into a consistent state.
+	     */
+        public Sphere()
+        {
+            diameter = 2.0;
+        }//end constructor
+
+        /**
+	     * Constructor accepts value of diameter.
+	     * Preferred constructor.
+	     * @param diameter
+	     */
+        public Sphere(double diameter)
+        {
+
+            this.setDiameter(diameter);
+
+        }//end constructor
+
+        /**
+	     * Calculates the volume of the sphere.
+	     * @return volume
+	     */
+        public double getVolume()
+        {
+
+            return (4.0 / 3.0) * Math.PI * Math.Pow(diameter / 2, 3);
+
+        }//end getVolume
+
+        /**
+	     * Calculates the surface area of the sphere.
+	     * @return - surface area
+	     */
+        public double getSurfaceArea()
+        {
+
+            return 4.0 * Math.PI * Math.Pow(diameter / 2, 2);
+        }//end getSurfaceArea
+
+        /**
+	     * Getter for diameter
+	     * @return
+	     */
+        public double getDiameter()
+        {
+            return diameter;
+        }//end getDiameter
+
+        /**
+	     * Setter for diameter. Checks bound on
+	     * formal parameter and resets to 2 if
+	     * the value is less than 2.
+	     * @param diameter
+	     */
+        public void setDiameter(double diameter)
+        {
+            if (diameter < 2)
+                this.diameter = 2;
+            else
+                this.diameter = diameter;
+        }//end setDiameter
+
+        public String toString()
+        {
+            return "Sphere [diameter=" + diameter + "]";
+        }//end toString
+    }//end class
+}//end namespace
---
+++ SphereProject/Sphere/TestSphere.cs
@@ -0,0 +1,47 @@
+﻿/**
+ * This class tests the methods of the sphere class
+ * @author Divya Manirajan
+ * @version 1.0
+ * Proj1-TestSphere
+ * SP20
+ */
+using System;
+
+namespace Sphere
+{
+    class TestSphere
+    {
+        public static void Main(string[] args)
+        {
+            //creates sphere with no set diameter
+            Sphere s1 = new Sphere();
+            //creates sphere with diameter of 2.5
+            Sphere s2 = new Sphere(2.5);
+
+            //prints out the volume of sphere 1
+            Console.WriteLine("The volume of sphere 1 is " + s1.getVolume());
+            //prints out the volume of sphere 2
+            Console.WriteLine("The volume of sphere 2 is " + s2.getVolume());
+
+            //prints out the surface area of sphere 1
+            Console.WriteLine("The surface area of sphere 1 is " + s1.getSurfaceArea());
+            //prints out the surface area of sphere 2
+            Console.WriteLine("The surface area of sphere 2 is " + s2.getSurfaceArea());
+
+            //sets the diameter of sphere 1 to 3
+            s1.setDiameter(3);
+
+            //prints out volume, surface area, and
+            //diameter of sphere 1 with the new diameter
+            Console.WriteLine("The diameter of sphere 1 is " + s1.getDiameter());
+            Console.WriteLine("The volume of sphere 1 is " + s1.getVolume());
+            Console.WriteLine("The surface area of sphere 1 is " + s1.getSurfaceArea());
+
+            //prints out information for both spheres using toString
+            Console.WriteLine(s1.toString());
+            Console.WriteLine(s2.toString());
+
+
+        }//end Main
+    }//end class
+}//end namespace
